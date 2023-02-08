bb7423328fc1336686ff44834a3dd528c8060ce4
SantiRuiz7 <ruizsantiagodavid@gmail.com>
2023-02-08 16:43:50 -0300

    agregado

--- 06_sets.py
+++ 06_sets.py
@@ -1,5 +1,14 @@
 ### Sets ###
 
 my_set = set()
+my_other_set = {}
 
 print(type(my_set))
+print(type(my_other_set)) # Iniciamente es un diccionario
+
+my_other_set = {"Brais", "Moure", "35"}
+print(type(my_other_set))
+
+print(len(my_other_set))
+
+print(my_other_set[0])
