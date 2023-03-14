4c0e841d76b1c5d2bb5c3f0dd9d05e07dbb322c6
SantiRuiz7 <ruizsantiagodavid@gmail.com>
2023-03-14 16:34:17 -0300

    mas Agregado

--- Intermediate/08_python_package_manager.py
+++ Intermediate/08_python_package_manager.py
@@ -2,4 +2,29 @@
 
 # PIP https://pypi.org
 
-import 
+import numpy # pip install numpy
+
+print(numpy.version.version)
+
+numpy_array = numpy.array([35, 24, 62, 52, 30, 30, 17])
+print(type(numpy_array))
+
+print(numpy_array * 2)
+
+import pandas # pip install pandas
+
+# pip list
+
+# pip install requests
+import requests
+
+response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=151")
+print(response)
+print(response.status_code)
+print(response.json())
+
+#Arithmetics Package
+
+from mypackage import arithmetics
+
+print(arithmetics.sum_two_values(1, 4))
