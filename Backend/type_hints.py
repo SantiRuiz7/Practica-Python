fedce3e6e2f501ccdeaae092ed05af0fe5578daf
SantiRuiz7 <ruizsantiagodavid@gmail.com>
2023-03-16 13:39:02 -0300

    Se agrega el Backend

---
+++ Backend/type_hints.py
@@ -0,0 +1,26 @@
+### Type Hints ###
+
+my_string_variable = "My String variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable = 5
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_typed_variable: str = "My typed String variable"
+print(my_typed_variable)
+print(type(my_typed_variable))
+
+my_typed_variable = 5
+print(my_typed_variable)
+print(type(my_typed_variable))
+
+my_typed_variable: str = 5
+my_typed_variable
+print(my_typed_variable)
+print(type(my_typed_variable))
+
+my_typed_variable = 5
+print(my_typed_variable)
+print(type(my_typed_variable))
